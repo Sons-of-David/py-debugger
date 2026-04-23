eebbb277c215731dcd437af7e1080aedbbcc2827
ofirdavid <eofirdavid@gmail.com>
2026-04-23 09:38:07 +0000

    delete old code

--- src/old-code/samples/feature-1-shapes_code.py
+++
@@ -1,2 +0,0 @@
-# No algorithm code here — this is a shapes showcase.
-# Just click Analyze to see the visual elements.
--- src/old-code/samples/feature-3-arrays_code.py
+++
@@ -1,2 +0,0 @@
-# No algorithm code here — this is an arrays showcase.
-# Just click Analyze to see the visual elements.
